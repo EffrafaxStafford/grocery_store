f4701b99ec2227b41a8541422baff6b6afebbb81
Abas Lampezhev <abas.lampezhev@gmail.com>
2026-01-29 13:00:40 +0300

    swagger

--- grocery_store/grocery_store/settings.py
+++ grocery_store/grocery_store/settings.py
@@ -98,5 +98,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 } 
